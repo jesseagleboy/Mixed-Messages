d1265a3b022f118839a78452076bc0496b9c2534
jesseagleboy <jslomowitz@gmail.com>
2021-06-08 12:58:43 -0400

    added fetch method

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+const fetch = require('node-fetch');
 const getQuote = async () => {
     const response = await fetch("https://zenquotes.io/api/quotes");
     if (response.ok) {
         const jsonResponse = await response.json();
-        console.log();
+        console.log(jsonResponse[0].q);
     }
 }
+
+getQuote();
