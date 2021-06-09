df99777752f2942ff5752300ce544fa485589df9
jesseagleboy <jslomowitz@gmail.com>
2021-06-09 03:05:54 -0400

    Went back to http

--- express.js
+++ express.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const app = express();
-const https = require('https');
-const fs = require('fs');
-var privateKey = fs.readFileSync('./localhost-key.pem');
-var certificate = fs.readFileSync('./localhost.pem');
 
 app.use('/', express.static('.'));
 
 const PORT = 3002;
 
-https.createServer({key: privateKey, cert: certificate}, app).listen(PORT);
+app.listen(PORT);
 
 
