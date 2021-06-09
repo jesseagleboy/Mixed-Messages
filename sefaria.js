61905898a863e424f6420bf187fd141d4a74e51e
jesseagleboy <jslomowitz@gmail.com>
2021-06-08 21:47:55 -0400

    Close to final edit

--- sefaria.js
+++ sefaria.js
@@ -4,17 +4,25 @@ const getData = async () => {
     //1. Have the list of Tanach books
     const tanach = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy', 'Joshua', 'Judges', ' I Samuel', 'II Samuel', 'I Kings', 'II Kings', 'Isaiah', 'Jeremiah', 'Ezekiel', 'Hosea', 'Joel', 'Amos', 'Obadiah', 'Jonah', 'Micah', 'Nahum', 'Habakkuk', 'Zephaniah', 'Haggai', 'Zechariah', 'Malachi', 'Psalms', 'Proverbs', 'Job', 'Song of Songs', 'Ruth', 'Lamentations', 'Ecclesiastes', 'Esther', 'Daniel', 'Ezra',  'Nehemiah', 'I Chronicles', 'II Chronicles'];
     //2. Get a book randomly
-    const book = tanach[Math.floor(Math.random() * 25)];
+    const book = tanach[Math.floor(Math.random() * tanach.length)];
     console.log(book);
+    
     //3. From the book, get the number of chapters of the book
     const chapters = await fetch(`https://www.sefaria.org/api/texts/${book}`);
     const chaptersJson = await chapters.json();
-    const chaptersLengths = chaptersJson.lengths[0];
-    //4. Once number of chapters are found, get one randomly
+    //Get number of chapters of book
+    const chaptersLengths = chaptersJson.length;
+    //Get number of verses from chapter using the length of the text array
+    const versesLengths = chaptersJson.text.length;
+
+    //Get the random values of chapter and verse
     const randomChapter = Math.floor(Math.random() * chaptersLengths);
-    //5. Once chapter is chosen, get number of verses for the chapter
-    const verses = await fetch(`https://www.sefaria.org/api/texts/${book}.${randomChapter}`);
-    console.log(verses);
+    const randomVerse = Math.floor(Math.random() * versesLengths);
+
+    //Concatenate the result
+    const finalVerseResponse = await fetch(`https://www.sefaria.org/api/texts/${book}.${randomChapter}`);
+    const finalVerseJson = await finalVerseResponse.json();
+    console.log(finalVerseJson.text[randomVerse]);
 
     
 }
