252852cc5b123f71495dfedbef98d92df8f58ad4
jesseagleboy <jslomowitz@gmail.com>
2021-06-08 22:08:14 -0400

    Working on if responses

--- sefaria.js
+++ sefaria.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-const getData = async () => {
+const getBibleVerse = async () => {
     //1. Have the list of Tanach books
     const tanach = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy', 'Joshua', 'Judges', ' I Samuel', 'II Samuel', 'I Kings', 'II Kings', 'Isaiah', 'Jeremiah', 'Ezekiel', 'Hosea', 'Joel', 'Amos', 'Obadiah', 'Jonah', 'Micah', 'Nahum', 'Habakkuk', 'Zephaniah', 'Haggai', 'Zechariah', 'Malachi', 'Psalms', 'Proverbs', 'Job', 'Song of Songs', 'Ruth', 'Lamentations', 'Ecclesiastes', 'Esther', 'Daniel', 'Ezra',  'Nehemiah', 'I Chronicles', 'II Chronicles'];
     //2. Get a book randomly
@@ -9,11 +9,13 @@ const getData = async () => {
     
     //3. From the book, get the number of chapters of the book
     const chapters = await fetch(`https://www.sefaria.org/api/texts/${book}`);
-    const chaptersJson = await chapters.json();
-    //Get number of chapters of book
-    const chaptersLengths = chaptersJson.length;
-    //Get number of verses from chapter using the length of the text array
-    const versesLengths = chaptersJson.text.length;
+    if (chapters.ok) {
+        const chaptersJson = await chapters.json();
+        //Get number of chapters of book
+        const chaptersLengths = chaptersJson.length;
+        //Get number of verses from chapter using the length of the text array
+        const versesLengths = chaptersJson.text.length;
+    }
 
     //Get the random values of chapter and verse
     const randomChapter = Math.floor(Math.random() * chaptersLengths);
@@ -21,10 +23,15 @@ const getData = async () => {
 
     //Concatenate the result
     const finalVerseResponse = await fetch(`https://www.sefaria.org/api/texts/${book}.${randomChapter}`);
-    const finalVerseJson = await finalVerseResponse.json();
-    console.log(finalVerseJson.text[randomVerse]);
+    if (finalVerseResponse.ok) {
+        const finalVerseJson = await finalVerseResponse.json();
+        return finalVerseJson.text[randomVerse];
 
+    }
+    
     
 }
 
-getData();
+getBibleVerse().then(response => console.log(response));
+
+module.exports = {getBibleVerse};
