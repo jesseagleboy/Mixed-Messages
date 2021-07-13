ee3e1a68463c7431ddfaa0af8594c64bd79f2336
Jesse Slomowitz <jslomowitz1@gmail.com>
2021-07-12 22:01:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const getQuote = async () => {
     let inspiringQuote = "";
     let bibleQuote = "";
     let asciiArt = "";
-    const response = await fetch('https://zenquotes.io/api/quotes');
+    const response = await fetch('https://www.zenquotes.io/api/quotes');
     console.log(`This is the response: ${response}`);
     if (response.ok) {
         console.log(response);
@@ -19,7 +19,7 @@ const getQuote = async () => {
     const plusQuote = bibleQuote.split(" ").join("+");
     const test = 'Hello, World!';
     
-    const asciiResponse = await fetch(`https://artii.herokuapp.com/make?text=${test}`);
+    const asciiResponse = await fetch(`https://www.artii.herokuapp.com/make?text=${test}`);
     if (asciiResponse.ok) {
         const jsonAscii = await asciiResponse.text();
         asciiArt = jsonAscii;
